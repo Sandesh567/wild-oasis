5f93998b8383fd37a0dfba8cf7fb18dd133fe5ae
sandesh <sandeshbhatta218@gmail.com>
2025-06-19 10:10:23 +0545

    Injecting Global Styles into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const H1 = styled.h1`
   font-size: 30px;
@@ -31,12 +32,15 @@ const StyledApp = styled.div`
 
 function App() {
   return (
-    <StyledApp>
-      <H1>The Wild Oasis</H1>
-      <Button onClick={() => alert("Check In")}>Check In</Button>
-      <Button onClick={() => alert("Check out")}>Check out</Button>
-      <Input type="number" placeholder="Number of Guests" />
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert("Check In")}>Check In</Button>
+        <Button onClick={() => alert("Check out")}>Check out</Button>
+        <Input type="number" placeholder="Number of Guests" />
+      </StyledApp>
+    </>
   );
 }
 
