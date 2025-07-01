d2eee1a4c03b3e706ce9241cdf12ba246ecbad58
sandesh <sandeshbhatta218@gmail.com>
2025-07-01 08:48:35 +0545

    Refactoring Input to a new file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
+import Input from "./ui/Input";
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  box-shadow: var(--shadow-sm);
-
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.div`
