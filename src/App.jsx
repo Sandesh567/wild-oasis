6bcb468e29b4e47c9750ad174ff69bc74732c20b
sandesh <sandeshbhatta218@gmail.com>
2025-07-02 09:21:41 +0545

    Installing react router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,5 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-const StyledApp = styled.div`
-  padding: 20px;
-`;
-
 function App() {
-  return (
-    <>
-      {/* when inspect it will show the same h1 tag in all but our type is different 
-    so to fix that styled components provides us "as" prop. as prop will be the element 
-    that will be rendered in the element  */}
-      <GlobalStyles />
-      <StyledApp>
-        <Row type="vertical">
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button onClick={() => alert("Check In")}>Check In</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("Check out")}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row type="vertical">
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of Guests" />
-              <Input type="number" placeholder="Number of Guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
-  );
+  return <div></div>;
 }
 
 export default App;
