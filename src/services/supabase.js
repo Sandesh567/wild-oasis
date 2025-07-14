45533b1f96ac6f5d682933a4bcbec6d9c5a66153
sandesh <sandeshbhatta218@gmail.com>
2025-07-14 10:11:45 +0545

    Exporting supabase.

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://eavajqpavntbhamunynq.supabase.co'
+export const supabaseUrl = 'https://eavajqpavntbhamunynq.supabase.co'
 //our project key
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdmFqcXBhdm50YmhhbXVueW5xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2ODQ5NDYsImV4cCI6MjA2NzI2MDk0Nn0.X7K6rMP8EmJFHLnIoVqXCBzA97uVm6X1JHiEwiWg8iU";
 const supabase = createClient(supabaseUrl, supabaseKey)
