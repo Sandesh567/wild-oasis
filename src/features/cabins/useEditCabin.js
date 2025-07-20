7f6834115ba5ed15e2f61cdaa50b4cd7fa2fd0f4
sandesh <sandeshbhatta218@gmail.com>
2025-07-20 20:34:29 +0545

    Fixing the editing bugs.

--- src/features/cabins/useEditCabin.js
+++ src/features/cabins/useEditCabin.js
@@ -1,18 +1,18 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createEditCabin } from "../../services/apiCabins";
-import toast from "react-hot-toast";
+import { toast } from "react-hot-toast";
 
 export function useEditCabin() {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const { mutate: editCabin, isLoading: isEditing } = useMutation({
-        mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
-        onSuccess: () => {
-            toast.success(" Cabin Successfully Edited");
-            queryClient.invalidateQueries({ queryKey: ["cabins"] });
+  const { mutate: editCabin, isLoading: isEditing } = useMutation({
+    mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
+    onSuccess: () => {
+      toast.success("Cabin successfully edited");
+      queryClient.invalidateQueries({ queryKey: ["cabins"] });
+    },
+    onError: (err) => toast.error(err.message),
+  });
 
-        },
-        onError: (err) => toast.error(err.message),
-    });
-    return { isEditing, editCabin };
+  return { isEditing, editCabin };
 }
